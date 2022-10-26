46f17356cd8fad6c47db97c3d095f0da4c5d82cd
mas-snowdemo <116715047+mas-snowdemo@users.noreply.github.com>
2022-10-26 11:09:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,11 @@
 import streamlit
 
-streamlit.title('My Mom's New Helathy Diner')
+streamlit.title('My Mom''s New Helathy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑Avocado Toast')
+
+streamlit.header('🍌🍓Build your own Smoothie🥝🍇')
