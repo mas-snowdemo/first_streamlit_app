26aedf2b2020d0c9d52d9331c821837f18415744
mas-snowdemo <116715047+mas-snowdemo@users.noreply.github.com>
2022-10-26 11:02:30 +0200

    Update streamlit_app.py
    added breackfast menu, header and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Helathy Diner')
+
+streamlit.header('Breakfast Menu');
+streamlit.text('Omega 3 & Blueberry Oatmeal');
+streamlit.text('Kale, SPinach & Rocket Smothie');
+streamlit.text('Hard-Boiled Free-Range Egg');
